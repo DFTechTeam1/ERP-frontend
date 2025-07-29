e1de3c15cd9b60339ce8930dd3e5e6f65cdff276
ilhammeru <gumilang.dev@gmail.com>
2025-07-29 15:49:10 +0700

    Fix(86b5wv981): Remove logging

--- src/compose/breakToken.js
+++ src/compose/breakToken.js
@@ -2,8 +2,6 @@ import { useEncrypt } from '@/compose/encrypt';
 
 export function useBreakToken(key, localStorageKey = 'dfauth') {
     const saltKey = import.meta.env.VITE_SALT_KEY;
-    console.log('saltKey', saltKey);
-    console.log('localStorageKey', localStorageKey);
     var encodedText = localStorage.getItem(localStorageKey);
     if (encodedText) {
         var { decodedString } = useEncrypt(encodedText, saltKey);
